45f3d83c6f57cbe99fb09414a51ef8ed96588bba
omisoul <marlonjames24@gmail.com>
2020-11-19 16:18:51 -0500

    Added city search functionality

--- world.js
+++ world.js
@@ -1,16 +1,27 @@
-let search = async () => {
+let search = async (isCitiesLookup) => {
     let searchInput = document.querySelector('#country');
     let result = document.querySelector('#result')
     country = searchInput.value
-    let res = await fetch(`world.php?country=${country}`);
-    let data = await res.text()
+    let res = '';
+    let data = '';
+    if(isCitiesLookup){
+        res = await fetch(`world.php?country=${country}&context=cities`);
+        data = await res.text()
+    }else{
+        res = await fetch(`world.php?country=${country}&context=country`);
+        data = await res.text()
+    }
     
     result.innerHTML = data;
 }
 window.addEventListener('load', () => {
     let btn = document.querySelector('#lookup');
-
+    let citBtn = document.querySelector('#cities');
     btn.addEventListener('click', () =>{
-        search()
+        search(false)
+    })
+
+    citBtn.addEventListener('click', () =>{
+        search(true)
     })
 })
--- world.php
+++ world.php
@@ -6,31 +6,71 @@ $dbname = 'world';
 
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
 
-if( $_GET["country"]){
-  $country = filter_var($_GET["country"], FILTER_SANITIZE_STRING);
-  $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
-}
-else{
-  $stmt = $conn->query("SELECT * FROM countries");
+if($_GET["context"] == 'cities'){
+  $cities = TRUE;
+  if( $_GET["country"]){ 
+    $country = filter_var($_GET["country"], FILTER_SANITIZE_STRING);
+    $stmt = $conn->query("SELECT 
+    cities.name,
+    cities.district, 
+    cities.population 
+    FROM cities
+    INNER JOIN countries
+    ON cities.country_code=countries.code
+    WHERE countries.name LIKE '%$country%';");
+  }
+  else{
+    $stmt = $conn->query("SELECT cities.name AS name, cities.district AS district, 
+    cities.population AS population FROM cities 
+     ");
+  }
+  
+}else if($_GET["context"] == 'country'){
+  $cities = FALSE;
+  if( $_GET["country"]){
+    $country = filter_var($_GET["country"], FILTER_SANITIZE_STRING);
+    $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
+  }
+  else{
+    $stmt = $conn->query("SELECT * FROM countries");
+  }
 }
 
-$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+  $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
 
 ?>
 <table>
-<tr>
-    <th>Name</th>
-    <th>Continent</th>
-    <th>Independence</th>
-    <th>Head of State</th>
-  </tr>
-<?php foreach ($results as $row): ?>
+<?php if($cities):?>
   <tr>
-    <td><?= $row['name'] ?></td>
-    <td><?= $row['continent'] ?></td>
-    <td><?= $row['independence_year'] ?></td>
-    <td><?= $row['head_of_state'] ?></td>
-  </tr>
- 
-<?php endforeach; ?>
+      <th>Name</th>
+      <th>District</th>
+      <th>Population</th>
+    </tr>
+  <?php foreach ($results as $row): ?>
+    <tr>
+      <td><?= $row['name'] ?></td>
+      <td><?= $row['district'] ?></td>
+      <td><?= $row['population'] ?></td>
+    </tr>
+  <?php endforeach; ?>
+<?php endif;?>
+
+<?php if(!$cities):?>
+  <tr>
+      <th>Name</th>
+      <th>Continent</th>
+      <th>Independence</th>
+      <th>Head of State</th>
+    </tr>
+  <?php foreach ($results as $row): ?>
+    <tr>
+      <td><?= $row['name'] ?></td>
+      <td><?= $row['continent'] ?></td>
+      <td><?= $row['independence_year'] ?></td>
+      <td><?= $row['head_of_state'] ?></td>
+    </tr>
+  <?php endforeach; ?>
+<?php endif;?>
+
 </table>
